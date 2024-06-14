9aa8e608b94456fc7c9c37adebaa7eb62bc62d4c
Bhoi Yedulal <155144819+rohittbhoi@users.noreply.github.com>
2024-06-14 23:40:27 +0530

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = 'db2943b68746236d6d8a86e98e8af86e';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
